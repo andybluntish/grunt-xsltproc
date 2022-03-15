34e8f7ed7bfc6283d65fe04b361c5f1120662ab2
Andy Stanford-Bluntish <andy@bluntish.net>
2022-03-16 00:08:26 +1030

    replace jshint with eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,8 @@
 module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
-    jshint: {
-      all: ["Gruntfile.js", "tasks/*.js", "<%= nodeunit.tests %>"],
-      options: {
-        jshintrc: ".jshintrc",
-      },
+    eslint: {
+      target: ["Gruntfile.js", "tasks/*.js", "test/*.js"],
     },
 
     // Before generating any new files, remove any previously-created files.
@@ -116,7 +113,7 @@ module.exports = function (grunt) {
   grunt.loadTasks("tasks");
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-eslint");
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-nodeunit");
 
@@ -125,5 +122,5 @@ module.exports = function (grunt) {
   grunt.registerTask("test", ["clean", "xsltproc", "nodeunit"]);
 
   // By default, lint and run all tests.
-  grunt.registerTask("default", ["jshint", "test"]);
+  grunt.registerTask("default", ["eslint", "test"]);
 };
